bd2841755d47c6a08f9ba3163ab8a8d34cc2562a
Myth-hack <hellomythsamanta@gmail.com>
2026-09-23 11:23:46 -0700

    Fix vite build by externalizing react-is

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,18 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react-is'],
+    },
+  },
+})
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
